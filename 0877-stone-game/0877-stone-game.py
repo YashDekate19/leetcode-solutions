255896dac4f71b75cb958e7561b28d02f62700db
Yash Dekate <yashdekate31@gmail.com>
2026-08-02 19:41:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (70.85%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        # p1 = 0
+        # p2 = 0
+
+        # for x in range(0,len(piles),2):
+
+        #     if(len(piles)== 2):
+        #         if(piles[0]>piles[1]):
+        #             p1+=piles.pop(0)
+        #             p2+=piles.pop(0)
+        #         else:
+        #             p1+=piles.pop(1)
+        #             p2+=piles.pop(0)
+
+        #     else:
+        #         if (piles[0] < piles[-1] and piles[1] <= piles[-2] and piles[-1]<=piles[-2]  ) or (piles[0] == piles[-1] and piles[1] > piles[-2] )\
+        #         or (piles[0] < piles[-1] and piles[1] >  piles[-2] and piles[-1] >  piles[1] and piles[-1] >  piles[-2]  ):
+        #             p1 += piles.pop(-1)
+        #         else:
+        #             p1 += piles.pop(0)
+
+        #         if (piles[0] < piles[-1] and piles[1] <= piles[-2] and piles[-1]<=piles[-2]  ) or (piles[0] == piles[-1] and piles[1] > piles[-2] )\
+        #         or (piles[0] < piles[-1] and piles[1] >  piles[-2] and piles[-1] >  piles[1] and piles[-1] >  piles[-2]  ) :
+        #             p2 += piles.pop(-1)
+        #         else:
+        #             p2 += piles.pop(0)
+        
+        return True
+
+        
