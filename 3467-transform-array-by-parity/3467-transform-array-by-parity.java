e57a7a61ced7d5c1b2120373cf6c1f2adaaf5382
Yash Dekate <yashdekate31@gmail.com>
2026-08-13 18:40:01 +0530

    Time: 1 ms (99.75%), Space: 46.7 MB (73.06%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        int count = 0;
+        for(int i =0 ; i<nums.length;i++){
+            if(nums[i]%2==0){
+                count++;
+            }
+        }
+
+        
+        for(int i =0 ; i<nums.length;i++){
+            if(i<count){
+                nums[i]=0;
+            }else{
+                nums[i]=1;
+            }
+        }
+
+        return nums;
+        
+    }
+}
